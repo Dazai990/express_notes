b7b3eb02ebba9a78af021e3e89486cbfb6a002a0
Dazai990 <157809815+Dazai990@users.noreply.github.com>
2025-02-17 13:30:17 +0530

    Delete static_middleware.js

--- static_middleware.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.use('/static',express.static("public"))
-router.use(express.static("files"))
-
-
-module.exports = router;
